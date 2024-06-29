6d8a38ad01cc40294b3a60c72971195d0b135de2
F0rgger <maxforger@gmail.com>
2024-06-29 23:01:22 +0300

    hw-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {FC} from 'react';
 import './App.css';
+import {ICharacter} from "./models/ICharacter";
+import {simpsons} from "./data";
+import Character from "./components/character/Character";
 
-const App = () => {
 
-dsdsd
+const App: FC = () => {
+
   return (
-      <div></div>
+      <div>
+        {
+          simpsons.map((character: ICharacter) =>
+              <Character character={character}>
+                {character.info}
+              </Character>)
+        }
+      </div>
   );
 }
+
 export default App;
