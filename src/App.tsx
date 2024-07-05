489abb6fd78db6bcce1021203c9a7b2391f528b6
F0rgger <maxforger@gmail.com>
2024-07-05 10:46:19 +0300

    hw-3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
 
-const App = () => {
+import React, { useState } from 'react';
+import useToggle from './hooks/useToggle';
+import usePrevious from './hooks/usePrevious';
 
+const ExampleComponent: React.FC = () => {
+  const [isToggled, toggle] = useToggle();
+  const [count, setCount] = useState(0);
+  const prevCount = usePrevious(count);
 
   return (
-      <div></div>
+      <div>
+        <button onClick={toggle}>
+          {isToggled ? 'ON' : 'OFF'}
+        </button>
+        <div>
+          <p>Current Count: {count}</p>
+          <p>Previous Count: {prevCount}</p>
+          <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
+      </div>
   );
-}
+};
 
-export default App;
+export default ExampleComponent;
